f8f5d18414c58e9014c766f9d7c82b3a9e5d3192
Amanda Hussey <amanda.k.hussey@gmail.com>
2025-08-19 18:09:08 -0600

    much better desktop and mobile

--- src/App.tsx
+++ src/App.tsx
@@ -5,58 +5,71 @@ import kleaningKweenWords from "./assets/kleaning-kween-words.png";
 import sparkles from "./assets/sparkles.png";
 
 function App() {
-  const isMobile = useMediaQuery("(max-width:900px)");
+  const isMobile = useMediaQuery("(max-width:600px)");
 
   return (
-    <Stack minHeight="100vh" p={3} justifyContent="space-between">
-      {isMobile && (
-        <img
-          src={fullBodyWithWords}
-          alt="The Kleaning Kween"
-          style={{
-            width: "100%",
-          }}
-        />
-      )}
+    <Stack
+      direction={isMobile ? "column" : "row-reverse"}
+      minHeight="100vh"
+      p={3}
+      spacing={8}
+      mb={10}
+      justifyContent={isMobile ? undefined : "center"}
+    >
+      {/* Image */}
+      <img
+        src={fullBodyWithWords}
+        alt="The Kleaning Kween"
+        style={{
+          width: isMobile ? "100%" : undefined,
+          height: isMobile ? undefined : "80vw",
+          maxHeight: 900,
+        }}
+      />
 
-      {/* Turn mess into magic + sparkles */}
-      <Stack direction="row" justifyContent="center" textAlign="center">
-        <Stack>
+      {/* All Text */}
+      <Stack justifyContent={isMobile ? "space-between" : "center"} spacing={3}>
+        {/* Turn mess into magic + sparkles */}
+        <Stack direction="row" justifyContent="center" textAlign="center">
+          <Stack>
+            <Typography>
+              I can turn <b>mess into magic</b>
+            </Typography>
+            <Typography>
+              with a touch of <b>class</b>
+            </Typography>
+            <Typography>
+              and a whole lot of{" "}
+              <b>
+                <i>sparkle</i>
+              </b>
+              .
+            </Typography>
+          </Stack>
+          <img
+            src={sparkles}
+            alt="sparkles"
+            style={{
+              width: 75,
+            }}
+          />
+        </Stack>
+
+        {/* Your space deserves */}
+        <Typography textAlign="center">
+          Your space deserves the <b>royal treatment</b>.
+        </Typography>
+
+        {/* Don't be shy */}
+        <Stack textAlign="center">
           <Typography>
-            I can turn <b>mess into magic</b>
-          </Typography>
-          <Typography>
-            with a touch of <b>class</b>
-          </Typography>
-          <Typography>
-            and a whole lot of{" "}
+            Don’t be shy — reach out and let’s make your place{" "}
             <b>
-              <i>sparkle</i>
+              <i>irresistibly</i> clean
             </b>
             .
           </Typography>
         </Stack>
-        <img
-          src={sparkles}
-          alt="sparkles"
-          style={{
-            width: 75,
-          }}
-        />
-      </Stack>
-
-      {/* Your space deserves */}
-      <Stack spacing={1} textAlign="center" mb={8}>
-        <Typography>
-          Your space deserves the <b>royal treatment</b>.
-        </Typography>
-        <Typography>
-          Don’t be shy — reach out and let’s make your place{" "}
-          <b>
-            <i>irresistibly</i> clean
-          </b>
-          .
-        </Typography>
       </Stack>
     </Stack>
   );
